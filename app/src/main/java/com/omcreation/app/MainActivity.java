ee7e58c9d4f0f7cb3a498b87d2651a0be457f30a
omcreationgroup <nsgabani@gmail.com>
2026-03-27 10:30:40 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/omcreation/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.omcreation.app;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.omcreationn.in");
+        setContentView(webView);
+    }
+}
